244e8b3bc06aa9fc599e545e4ebab26c54aabf2f
pgm-behlsava <behlsava@student.arteveldehs.be>
2024-01-23 17:29:09 +0100

    adjusting header logos paths, v2

--- static/js/logos.js
+++ static/js/logos.js
@@ -3,7 +3,8 @@ export {animateRandomCampaignLogo, animateRandomHeaderLogo};
 const url = new URL(location.href);
 
 function animateRandomHeaderLogo($elements, number) {
-  // if (url.pathname !== '/events/day.html') {
+  
+  if (url.pathname !== '/opdracht-2-gentse-feesten-pgm-behlsava/events/day.html') {
     $elements.forEach(($element) => {
       if (number < 0.16)
         $element.setAttribute(
@@ -36,47 +37,49 @@ function animateRandomHeaderLogo($elements, number) {
           "static/img/logos/GF-logo-2023-6-E.svg"
         );
     });
-  // } 
-  
-  // else {
-  //   $elements.forEach(($element) => {
-  //     if (number < 0.16)
-  //       $element.setAttribute(
-  //         "src",
-  //         "../static/img/logos/GF-logo-2023-1-G.svg"
-  //       );
-  //     else if (number >= 0.16 && number < 0.32)
-  //       $element.setAttribute(
-  //         "src",
-  //         "../static/img/logos/GF-logo-2023-2-E.svg"
-  //       );
-  //     else if (number >= 0.32 && number < 0.48)
-  //       $element.setAttribute(
-  //         "src",
-  //         "../static/img/logos/GF-logo-2023-3-N.svg"
-  //       );
-  //     else if (number >= 0.48 && number < 0.64)
-  //       $element.setAttribute(
-  //         "src",
-  //         "../static/img/logos/GF-logo-2023-4-T.svg"
-  //       );
-  //     else if (number >= 0.64 && number < 0.8)
-  //       $element.setAttribute(
-  //         "src",
-  //         "../static/img/logos/GF-logo-2023-5-S.svg"
-  //       );
-  //     else
-  //       $element.setAttribute(
-  //         "src",
-  //         "../static/img/logos/GF-logo-2023-6-E.svg"
-  //       );
-  //   });
-  // }
+  } 
+
+  else {
+    
+    $elements.forEach(($element) => {
+      if (number < 0.16)
+        $element.setAttribute(
+          "src",
+          "../static/img/logos/GF-logo-2023-1-G.svg"
+        );
+      else if (number >= 0.16 && number < 0.32)
+        $element.setAttribute(
+          "src",
+          "../static/img/logos/GF-logo-2023-2-E.svg"
+        );
+      else if (number >= 0.32 && number < 0.48)
+        $element.setAttribute(
+          "src",
+          "../static/img/logos/GF-logo-2023-3-N.svg"
+        );
+      else if (number >= 0.48 && number < 0.64)
+        $element.setAttribute(
+          "src",
+          "../static/img/logos/GF-logo-2023-4-T.svg"
+        );
+      else if (number >= 0.64 && number < 0.8)
+        $element.setAttribute(
+          "src",
+          "../static/img/logos/GF-logo-2023-5-S.svg"
+        );
+      else
+        $element.setAttribute(
+          "src",
+          "../static/img/logos/GF-logo-2023-6-E.svg"
+        );
+    });
+  }
   
 }
 
 function animateRandomCampaignLogo($elements, number) {
-  if (url.pathname !== '/events/day.html') {
+
+  if (url.pathname !== '/opdracht-2-gentse-feesten-pgm-behlsava/events/day.html') {
     $elements.forEach(($element) => {
       if (number < 0.16)
         $element.setAttribute(
@@ -109,7 +112,9 @@ function animateRandomCampaignLogo($elements, number) {
           "static/img/logos/campaign-logos/campagne-6-E.png"
         );
     });
-  } else {
+  } 
+  else {
+    
     $elements.forEach(($element) => {
       if (number < 0.16)
         $element.setAttribute(
